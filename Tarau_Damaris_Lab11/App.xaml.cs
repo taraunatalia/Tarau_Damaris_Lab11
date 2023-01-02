cd522f601b8f32e1d7167c530e71373f131800d9
Damaris Tarau <taraudamaris@yahoo.com>
2023-01-02 16:46:20 +0200

    Add project files.

---
+++ Tarau_Damaris_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Tarau_Damaris_Lab11.Data;
+
+namespace Tarau_Damaris_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Tarau_Damaris_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tarau_Damaris_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
